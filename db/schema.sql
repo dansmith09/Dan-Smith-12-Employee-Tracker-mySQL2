959bfc04f0c7e74b98f28246ed857f76d3c137a2
dansmith09 <dansmith_09@hotmail.com>
2022-08-03 14:58:20 +1000

    database set up

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db;
+
+USE staff_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY department_id
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT, 
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  -- Figure out how to link an employee to another employee to establish manager relationship
+  
+)
